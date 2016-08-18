1670c427b959a70419f78474637ce620ce525fd9
linouxis9 <linouxis9@gmail.com>
2016-08-18 21:24:50 +0200

    Update to the new server

--- OpenLedger.php
+++ OpenLedger.php
@@ -22,7 +22,7 @@ class OpenLedger
 {
 	public function __construct($account){
 		$this->name = $account;
-		$this->API = new OpenLedgerAPI("http://185.62.189.242/graphene-php/api/v1/");
+		$this->API = new OpenLedgerAPI("http://91.121.153.72/bts/api/v1/");
 		$this->Wallet = new OpenLedgerWallet($this);
 		$this->DEX = new OpenLedgerDEX($this);
 		$this->Asset = new OpenLedgerAsset($this);
